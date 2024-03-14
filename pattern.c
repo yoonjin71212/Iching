84ca024c77d4827610ff7e1f7fd93081bd777aa4
Yunjin Lee <yoonjin67@gmail.com>
2024-03-14 22:56:34 +0900

    PATTERN

--- pattern.c
+++ pattern.c
@@ -1,42 +1,33 @@
 #include <stdio.h>
 #include <string.h>
-#include<stdlib.h>
 #include <math.h>
 #define PI 3.1415926535897
 #define HEIGHT_WIDTH 1000
 typedef struct BMPSIN BMPSIN;
 static unsigned char rgb[HEIGHT_WIDTH][HEIGHT_WIDTH];
 int a=0,b=0,c=0;
-int yi(int *x, int *y) {
-    if ((*x)<(*y))
-    {
-        (*x)++;
-        (*y)--;
-        return *x;
-    } else if ((*x)>(*y)) {
-        (*y)++;
-        (*x)++;
-        return (*x)+(*y);
-    } else if((*x)==(*y)) {
-        (*y)++;
-        return *y;
-    }
-}
 void pattern(int count, int x, int y)
 {
-    int *res=malloc(sizeof(int));
+    if(x==1000000000||y==1000000000) {
+        return;
+    }
     while (1)
     {
-back:
-        if(*res==1) {
-            *res+=yi(&x,&y);
-        } else {
-            *res = yi(&x,&y);
+        if (x<y)
+        {
+            x++;
+            y--;
+            printf("%d  %d   ",x,y);
+        } else if (x>y) {
+            y++;
+            x++;
+            printf("%d  %d   ",y,x);
+        } else if(x==y) {
+            y++;
+            printf("%d  %d   ",y,x);
         }
-        printf("%d \n", *res);
     }
 }
-
 
 int main(int argc, char **argv)
 {
