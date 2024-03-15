7c042937d315043a09ad4999b9188f3a95e2a94a
Yunjin Lee <yoonjin67@gmail.com>
2024-03-15 13:07:35 +0900

    chaoticrandom

--- pattern.c
+++ pattern.c
@@ -1,65 +1,41 @@
 #include <stdio.h>
 #include <string.h>
-#include<stdlib.h>
+#include <unistd.h>
 #include <math.h>
 #define PI 3.1415926535897
 #define HEIGHT_WIDTH 1000
 typedef struct BMPSIN BMPSIN;
+FILE *f;
 static unsigned char rgb[HEIGHT_WIDTH][HEIGHT_WIDTH];
-long long a=0,b=0,c=0;
-long long idx = 1;
-int yi(int *x, int *y) {
-    if ((*x)<(*y))
-    {
-        (*x)++;
-        (*y)--;
-        return *x;
-    } else if ((*x)>(*y)) {
-        (*y)++;
-        (*x)++;
-        return (*x)+(*y);
-    } else if((*x)==(*y)) {
-        (*x)++;
-        return *y;
-    }
-}
+int a=0,b=0,c=0;
 void pattern(int count, int x, int y)
 {
-    long long *res=malloc(sizeof(int));
-    int prev = 0;
+    if(x==10000000||y==10000000) {
+        return;
+    }
     while (1)
     {
-        if(*res==0) {
-            *res+=yi(&x,&y);
-        } else {
-            back:
-            *res = yi(&x,&y);
+        if (x<y)
+        {
+            x++;
+            y--;
+            fprintf(f,"%d %d",x,y);
+        } else if (x>y) {
+            y++;
+            x++;
+            fprintf(f,"%d %d",y,x);
+        } else if(x==y) {
+            y++;
+            fprintf(f,"%d %d",y,x);
         }
-
-        if ((*res%3)==0) {
-            goto back;
-        }
-        if((*res%5)==0) {
-            goto back;
-        }
-        if((*res%7)==0) {
-            goto back;
-        }
-        if((*res%9)==0) {
-            goto back;
-        }
-        printf("%lld \n", *res);
+        sleep(0.1);
     }
 }
-
 
 int main(int argc, char **argv)
 {
-  printf("2 \n");
-  printf("3 \n");
-  printf("5 \n");
-  printf("7 \n");
+  f=fopen("./test.txt", "wt");
   pattern(a,b,c);
-	return 0;
+  return 0;
+  fclose(f);
 }
-
